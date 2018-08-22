c038d62361c531a98aeb307dea8b658fdbb75484
subhash <snsubhash78@gmail.com>
2018-08-22 15:18:19 +0530

    Modification Drawer component

--- src/components/Drawer/drawerContentComponents.js
+++ src/components/Drawer/drawerContentComponents.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {NavigationActions} from 'react-navigation';
-import { Text, View, StyleSheet, ImageBackground } from 'react-native'
+import { Text, View, StyleSheet, ImageBackground, Image } from 'react-native'
 
 export default class drawerContentComponents extends Component {
 
@@ -16,20 +16,23 @@ export default class drawerContentComponents extends Component {
     return (
         <View style={styles.container}>
             <View style={styles.headerContainer}>
-                <ImageBackground source={require('../../../assets/drawer-cover.png')} style={{flex: 1, width: 280, justifyContent: 'center'}} >
-                    <Text style={styles.headerText}>Header Portion</Text>
-                    <Text style={styles.headerText}>You can display here logo or profile image</Text>
+                <ImageBackground source={require('../../../assets/drawer-cover.png')} style={{flex: 1, width: 280, justifyContent: 'center',alignItems: 'center'}} >
+                <Image source = {require('../../../assets/app/pepboys1.png')} style={styles.CircleShapeImageView}/>
+                    <Text style={styles.headerText}>Steve John</Text>
                 </ImageBackground>
             </View>
             <View style={styles.screenContainer}>
                 <View style={styles.screenStyle}>
-                    <Text onPress={this.navigateToScreen('ScreenA')}>Screen A</Text>
+                    <Image source = {require('../../../assets/app/home.png')} style={{ width: 25, height: 25, marginLeft: 5}}/>
+                    <Text onPress={this.navigateToScreen('ScreenA')} style={styles.screenTextContainer}>Home</Text>
                 </View>
                 <View style={styles.screenStyle}>
-                    <Text onPress={this.navigateToScreen('ScreenB')}>Screen B</Text>
+                    <Image source = {require('../../../assets/app/home.png')} style={{ width: 25, height: 25, marginLeft: 5}}/>
+                    <Text onPress={this.navigateToScreen('ScreenB')} style={styles.screenTextContainer}>Screen B</Text>
                 </View>
                 <View style={styles.screenStyle}>
-                    <Text onPress={this.navigateToScreen('ScreenC')}>Screen C</Text>
+                    <Image source = {require('../../../assets/app/home.png')} style={{ width: 25, height: 25, marginLeft: 5}}/>
+                    <Text onPress={this.navigateToScreen('ScreenC')} style={styles.screenTextContainer}>Screen C</Text>
                 </View>
             </View>
         </View>
@@ -41,15 +44,19 @@ export default class drawerContentComponents extends Component {
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
+        width: 200
     },
     headerContainer: {
-        height: 150,
+        height: 200,
+        width: 200
     },
     headerText: {
         color: '#fff8f8',
+        paddingTop:15
     },
     screenContainer: {
-        paddingTop: 20
+        paddingTop: 20,
+        width: 200
     },
     screenStyle: {
         height: 30,
@@ -61,5 +68,15 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginLeft: 20
     },
+    screenTextContainer:{
+        paddingLeft: 10
+    },
+    CircleShapeImageView: {
+        width: 80,
+        height: 80,
+        borderRadius: 80/2,
+        backgroundColor: '#00BCD4',
+        paddingLeft: 40
+    }
 
 });
